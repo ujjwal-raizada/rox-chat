3bf9768e55764214c518426f6362d2ade8f7e2a2
Ujjwal Raizada <ujjwalraizada@gmail.com>
2018-06-09 02:51:56 +0530

    username: user can now select usernames

--- client.py
+++ client.py
@@ -15,17 +15,25 @@ intro = """
 
 print(intro)
 host = '127.0.0.1'
-port = 5004
+port = 5006
  
+
+
 mySocket = socket.socket()
 mySocket.connect((host,port))
 
-
 def MsgPrint():
+    usernameSet = False
     while True:
         data = mySocket.recv(1024).decode()
         if data:
-            print ('Received from server: ' + data)
+            if usernameSet:
+                print(data)
+            if data == 'Select your Username: ':
+                print(data)
+            if data == 'Username Registered!!':
+                print(data)
+                usernameSet = True
         else:
             break
 
@@ -39,12 +47,11 @@ def Main():
     thread.start()
 
     print("Print thread created")
-     
-    message = input(" -> ")
-     
+    message = input()
+
     while message != 'q':
-            mySocket.send(message.encode())
-            message = input(" -> ")
+        mySocket.send(message.encode())
+        message = input()
              
     mySocket.close()
  
--- server.py
+++ server.py
@@ -5,25 +5,46 @@ from queue import Queue
 import time
 
 
-q = Queue()
+intro = """
+██████╗  ██████╗ ██╗  ██╗ ██████╗██╗  ██╗ █████╗ ████████╗
+██╔══██╗██╔═══██╗╚██╗██╔╝██╔════╝██║  ██║██╔══██╗╚══██╔══╝
+██████╔╝██║   ██║ ╚███╔╝ ██║     ███████║███████║   ██║   
+██╔══██╗██║   ██║ ██╔██╗ ██║     ██╔══██║██╔══██║   ██║   
+██║  ██║╚██████╔╝██╔╝ ██╗╚██████╗██║  ██║██║  ██║   ██║   
+╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   
+
+    """
+
+print(intro)
 
 
 def ThreadCount():
-    print(threading.active_count())
+    print("No. of active threads: "+str(threading.active_count()))
 
 def server(conn, addr, id):
-    print("connected:" + str(addr) + "id:"+ str(id))
+    print("connected:" + str(addr) + " id:"+ str(id))
+
+
+    Ask = "Select your Username: "
+    conn.send(Ask.encode())
+    Username = conn.recv(1024).decode()
+    userConfirm = "Username Registered!!"
+    conn.send(userConfirm.encode())
+
 
     while True:
         data = conn.recv(1024).decode()
         if not data:
                 break
-        print ("from connected  user: " + str(data))
-         
-        data = str(data).upper()
-        print ("sending: " + str(data))
-        for c in tconn:
-            c.send(data.encode())
+        print ("from "+Username+": " + str(data))
+        print ("Broadcasting: " + str(data))
+        for con in conList:
+            try:
+                message = Username+": "+data
+                con.send(message.encode())
+            except:
+                pass
+
 
              
     conn.close()
@@ -33,11 +54,11 @@ def server(conn, addr, id):
 
 
 thread = []
-tconn = [] 
+conList = []
 def Main():
     i = 0
     host = "127.0.0.1"
-    port = 5004
+    port = 5006
      
     mySocket = socket.socket()
     mySocket.bind((host,port))
@@ -50,13 +71,11 @@ def Main():
         conn, addr = mySocket.accept()
         print ("Connection from: " + str(addr))
         thread.append(Thread(target=server, args=(conn, addr, i)) )
+        thread[i].deamon = True
         thread[i].start()
-        print("thread: "+str(i)+" created")
-        tconn.append(conn)
-        print("tcon: "+str(tconn))
+        print("thread_id: "+str(i)+" created")
+        conList.append(conn)
         i+=1
-
-
 
 
 
