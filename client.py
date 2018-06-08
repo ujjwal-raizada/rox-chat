17af70021a57d0dcdf953e50c9ca1d49bdeac054
Ujjwal Raizada <ujjwalraizada@gmail.com>
2018-06-09 01:18:25 +0530

    threading: Multi-threading for multiple user chat

--- client.py
+++ client.py
@@ -1,23 +1,52 @@
 import socket
+from threading import Thread
+import time
+
+
+intro = """
+██████╗  ██████╗ ██╗  ██╗ ██████╗██╗  ██╗ █████╗ ████████╗
+██╔══██╗██╔═══██╗╚██╗██╔╝██╔════╝██║  ██║██╔══██╗╚══██╔══╝
+██████╔╝██║   ██║ ╚███╔╝ ██║     ███████║███████║   ██║   
+██╔══██╗██║   ██║ ██╔██╗ ██║     ██╔══██║██╔══██║   ██║   
+██║  ██║╚██████╔╝██╔╝ ██╗╚██████╗██║  ██║██║  ██║   ██║   
+╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   
+
+    """
+
+print(intro)
+host = '127.0.0.1'
+port = 5004
+ 
+mySocket = socket.socket()
+mySocket.connect((host,port))
+
+
+def MsgPrint():
+    while True:
+        data = mySocket.recv(1024).decode()
+        if data:
+            print ('Received from server: ' + data)
+        else:
+            break
+
+
+
  
 def Main():
-        host = '127.0.0.1'
-        port = 5000
-         
-        mySocket = socket.socket()
-        mySocket.connect((host,port))
-         
-        message = input(" -> ")
-         
-        while message != 'q':
-                mySocket.send(message.encode())
-                data = mySocket.recv(1024).decode()
-                 
-                print ('Received from server: ' + data)
-                 
-                message = input(" -> ")
-                 
-        mySocket.close()
+
+    thread = Thread(target=MsgPrint, args=(()))
+    thread.deamon = True
+    thread.start()
+
+    print("Print thread created")
+     
+    message = input(" -> ")
+     
+    while message != 'q':
+            mySocket.send(message.encode())
+            message = input(" -> ")
+             
+    mySocket.close()
  
 if __name__ == '__main__':
     Main()
--- server.py
+++ server.py
@@ -1,26 +1,64 @@
 import socket
- 
+import threading
+from threading import Thread
+from queue import Queue
+import time
+
+
+q = Queue()
+
+
+def ThreadCount():
+    print(threading.active_count())
+
+def server(conn, addr, id):
+    print("connected:" + str(addr) + "id:"+ str(id))
+
+    while True:
+        data = conn.recv(1024).decode()
+        if not data:
+                break
+        print ("from connected  user: " + str(data))
+         
+        data = str(data).upper()
+        print ("sending: " + str(data))
+        for c in tconn:
+            c.send(data.encode())
+
+             
+    conn.close()
+    print("Thread Died: "+str(id))
+
+
+
+
+thread = []
+tconn = [] 
 def Main():
+    i = 0
     host = "127.0.0.1"
-    port = 5000
+    port = 5004
      
     mySocket = socket.socket()
     mySocket.bind((host,port))
      
-    mySocket.listen(1)
-    conn, addr = mySocket.accept()
-    print ("Connection from: " + str(addr))
+    
+
     while True:
-            data = conn.recv(1024).decode()
-            if not data:
-                    break
-            print ("from connected  user: " + str(data))
-             
-            data = str(data).upper()
-            print ("sending: " + str(data))
-            conn.send(data.encode())
-             
-    conn.close()
-     
+        ThreadCount()
+        mySocket.listen(1)
+        conn, addr = mySocket.accept()
+        print ("Connection from: " + str(addr))
+        thread.append(Thread(target=server, args=(conn, addr, i)) )
+        thread[i].start()
+        print("thread: "+str(i)+" created")
+        tconn.append(conn)
+        print("tcon: "+str(tconn))
+        i+=1
+
+
+
+
+
 if __name__ == '__main__':
     Main()
